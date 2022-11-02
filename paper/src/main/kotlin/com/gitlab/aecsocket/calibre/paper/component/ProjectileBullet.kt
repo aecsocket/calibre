63dbad4806cf9a38cfe3c74584a3c5220b63e6f4
aecsocket <aecsocket@tutanota.com>
2022-11-02 18:33:08 +0000

    Start on projectiles

---
+++ paper/src/main/kotlin/com/gitlab/aecsocket/calibre/paper/component/ProjectileBullet.kt
@@ -0,0 +1,41 @@
+package com.gitlab.aecsocket.calibre.paper.component
+
+import com.gitlab.aecsocket.alexandria.paper.extension.key
+import com.gitlab.aecsocket.calibre.paper.CalibreAPI
+import com.gitlab.aecsocket.sokol.core.*
+import com.gitlab.aecsocket.sokol.paper.component.PositionWrite
+import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+
+class ProjectileBullet(val profile: Profile) : PersistentComponent {
+    companion object {
+        val Key = CalibreAPI.key("projectile_bullet")
+        val Type = ComponentType.deserializing<Profile>(Key)
+    }
+
+    override val componentType get() = ProjectileBullet::class
+    override val key get() = Key
+
+    override fun write(ctx: NBTTagContext) = ctx.makeCompound()
+
+    override fun write(node: ConfigurationNode) {}
+
+    @ConfigSerializable
+    data class Profile(
+        val a: Boolean = false,
+    ) : NonReadingComponentProfile {
+        override fun readEmpty() = ProjectileBullet(this)
+    }
+}
+
+@All(ProjectileBullet::class, PositionWrite::class)
+class ProjectileBulletSystem(mappers: ComponentIdAccess) : SokolSystem {
+    private val mProjectileBullet = mappers.componentMapper<ProjectileBullet>()
+    private val mPositionWrite = mappers.componentMapper<PositionWrite>()
+
+    @Subscribe
+    fun on(event: SokolEvent.Update, entity: SokolEntity) {
+        val projectileBullet = mProjectileBullet.get(entity)
+        val positionWrite = mPositionWrite.get(entity)
+    }
+}
